dab521d10fbe95de0c5a16a3692ac759b1cac29c
Qiki <qi@brandingbrand.com>
2018-07-16 23:20:55 -0400

    learned aync

--- app/async.js
+++ app/async.js
@@ -2,10 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(resolve => {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise(resolve => {
+      $.get(url, data => {
+        let result  = data.people.map(p => {
+          return p.name;
+        });
+        result.sort();
+        resolve(result);
+      })
+    })
   }
 };
